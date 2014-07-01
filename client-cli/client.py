5b2eb1843caacae505b6a9af7163b69092bf5e44
Patrick Uiterwijk <puiterwijk@redhat.com>
2014-07-01 07:32:18 -0400

    Finish the rename to webSilvia

--- client-cli/client.py
+++ client-cli/client.py
@@ -1,20 +1,20 @@
 # Copyright (c) 2014, Patrick Uiterwijk <puiterwijk@gmail.com>
 # All rights reserved.
 #
-# This file is part of pySilvia.
+# This file is part of webSilvia.
 #
-# pySilvia is free software: you can redistribute it and/or modify
+# webSilvia is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# pySilvia is distributed in the hope that it will be useful,
+# webSilvia is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with pySilvia.  If not, see <http://www.gnu.org/licenses/>.
+# along with webSilvia.  If not, see <http://www.gnu.org/licenses/>.
 
 # Please configure the path to the silvia_proxy binary
 PROXY_PATH = '/usr/bin/silvia_proxy'
---
+++ server/gunicorn_config.py
@@ -0,0 +1,29 @@
+# Copyright (c) 2014, Patrick Uiterwijk <puiterwijk@gmail.com>
+# All rights reserved.
+#
+# This file is part of webSilvia.
+#
+# webSilvia is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# webSilvia is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with webSilvia.  If not, see <http://www.gnu.org/licenses/>.
+
+# You can not increase this from one for now, as we use global variables
+workers = 1
+worker_class = 'socketio.sgunicorn.GeventSocketIOWorker'
+bind = '0.0.0.0:5000'
+pidfile = '/opt/webSilvia/server/gunicorn.pid'
+debug = True
+loglevel = 'debug'
+errorlog = '/opt/webSilvia/server/gunicorn.log'
+daemon = True
+# keyfile = '/opt/webSilvia/silvia.fedoauth.org.key'
+# certfile = '/opt/webSilvia/silvia.fedoauth.org.pem'
